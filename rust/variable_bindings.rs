c7f4b4592984dcb84eaa308c85cc6c95fc73a336
charity-g <charity.grey1@gmail.com>
2026-07-04 08:30:21 -0600

    add var_bindings

---
+++ rust/variable_bindings.rs
@@ -0,0 +1,40 @@
+fn var_bindings() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u32;
+
+    let _noisy_unused_variable = 2u32;
+    // FIXME ^ Prefix with an underscore to suppress the warning
+    // Please note that warnings may not be shown in a browser
+}
+
+fn mutability() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error! Cannot assign a new value to an immutable variable
+    // _immutable_binding += 1;
+}
+
+fn main() {
+    var_bindings();
+    mutability();
+}
