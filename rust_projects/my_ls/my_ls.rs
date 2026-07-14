7f1e9cb551c3fe8bf04a348c528ac7b080673ec3
charity-g <charity.grey1@gmail.com>
2026-07-13 21:40:09 -0700

    add in desc

--- rust_projects/my_ls/my_ls.rs
+++ rust_projects/my_ls/my_ls.rs
@@ -1,6 +1,21 @@
 //! rustc my_ls.rs && ./my_ls -lah
 use std::env;
     
+
+fn collect_folder(a_flag: bool, h_flag: bool, l_flag: bool) {
+    println!("uninplemented");
+}
+
+/**
+ * Command line usage: 
+ * Root folder to start from: assumes user is asking about current directory
+ *
+ * Flags -lah in any order, or none existent. 
+ *  a = all = show hidden files
+ *  h = human-readable
+ *  l = long format = Show detailed information
+ *    
+*/ 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
@@ -16,5 +31,7 @@ fn main() {
     let h_flag : bool = flags.contains(&'h');
     let l_flag : bool = flags.contains(&'l');
 
-    dbg!(a_flag,h_flag,l_flag);
+    // dbg!(a_flag,h_flag,l_flag);
+
+    collect_folders();
 }
