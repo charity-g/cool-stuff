453eaf70ed89492c758493c4f2cb1ec0b8aa7df1
charity-g <charity.grey1@gmail.com>
2026-07-14 22:27:37 -0700

    add struct for -l flag

--- rust_projects/my_ls/my_ls.rs
+++ rust_projects/my_ls/my_ls.rs
@@ -1,7 +1,32 @@
 //! rustc my_ls.rs && ./my_ls -lah
-use std::env;
+
 use std::fs;
+use std::env;
+use std::fmt;
 use std::ffi::OsStr;
+use chrono::{DateTime, Utc};
+use std::os::unix::fs::MetadataExt;
+
+#[derive(Debug)]
+struct LongFormatAttributes {
+    ftype_perms: String,
+    hard_links: u64,
+    owner_name: String,
+    group_name: String,
+    fsize_bytes: u64,
+    last_mod_time: String,
+    filename: String,
+}
+
+impl fmt::Display for LongFormatAttributes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(
+            f, "{} {} {} {} {} {} {}",  
+            self.ftype_perms, self.hard_links, self.owner_name, self.group_name, self.fsize_bytes, self.last_mod_time, self.filename)
+    }
+}
+
 
 fn is_hidden_file(file_name: &OsStr) -> bool {
     
@@ -11,6 +36,12 @@ fn is_hidden_file(file_name: &OsStr) -> bool {
     filename_str.starts_with(".")    
 }
 
+fn convert_date(time: i64) -> String {
+    let Some(datetime) = DateTime::from_timestamp(seconds, time) else {
+        return "".to_string();
+    };
+    datetime.format("%H:%M:%S").to_string()
+}
 
 
 fn collect_folders(a_flag: bool, h_flag: bool, l_flag: bool) {
@@ -29,14 +60,29 @@ fn collect_folders(a_flag: bool, h_flag: bool, l_flag: bool) {
         if is_hidden_file(&entry.file_name()) && !a_flag {
             continue;
         }
+        
+        if !l_flag {
+            println!("{}", entry.file_name().into_string().unwrap());
+        } else {
+            let long_format: LongFormatAttributes = LongFormatAttributes {
+                ftype_perms:    metadata.mode(), //TODO fix to string instead of u32
+                hard_links:     metadata.nlink(),
+                owner_name:     metadata.uid(), //TODO get owner name instead of id
+                group_name:     metadata.gid(), // TODO get group name instead of i
+                fsize_bytes:    metadata.len(),
+                last_mod_time:  convert_date(metadata.mtime()),
+                filename:       (&entry.file_name().to_str().unwrap()).to_string(),           
+            };
 
-        println!(
-            "{} {} {} bytes {:?}",
-            entry.file_name().into_string().unwrap(),
-            path.display(),
-            metadata.len(),
-            metadata,
-        );
+        }
+
+     //   println!(
+     //       "{} {} {} bytes {:?}",
+     //       entry.file_name().into_string().unwrap(),
+     //       path.display(),
+     //       metadata.len(),
+     //       metadata,
+     //   );
     }
     // note: you cannot collect this: println!("uninplemented {:?}", dirEntries);
 }
